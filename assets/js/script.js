0f5489f2df6f4ac0c2e69c65cb76502dda06be07
Jingsi Gong <gongjingsi@hotmail.com>
2022-07-19 00:28:03 -0400

    update generatePassword function based on lowercase

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,39 @@ var randomNumber = function(min, max) {
 
 // Generate password based on criteria
 var generatePassword = function(){
-
-  var pwLength = window.prompt("Enter a number for the length of password at least 8 characters and no more than 128 characters. For example, 8.")
+  var criteria = window.prompt("Choose which criteria to include in the password. Enter 'Length' or 'Lowercase' or 'Uppercase' or 'Special Character' to choose. ");
   
-  if (Number.isInteger(parseInt(pwLength))) {
-    
-    password = new Array();
+  if (criteria == "Length") {
+    var pwLength = window.prompt("Enter a number for the length of password at least 8 characters and no more than 128 characters. For example, 8.")
+  
+    if (Number.isInteger(parseInt(pwLength))) {
+      
+      password = new Array();
 
-    for(let i=0; i<pwLength; i++){
-      password[i] = randomNumber(0, 9);
+      for(let i=0; i<pwLength; i++){
+        password[i] = randomNumber(0, 9);
+      }
+      return password.toString().replace(/,/g, '');
+
+    } else {
+      window.alert("You did not enter a number. Try again.");
+
     }
-    return password.toString().replace(/,/g, '');
-
-  } else {
-    window.alert("You did not enter a number. Try again.");
-    
+  } else if (criteria == "Lowercase") {
+    var confirmLowercase = window.confirm("Would you like to include lowercase in password?");
+     
+    if (confirmLowercase) {
+      var pwLetter = " ";
+      var characters = "abcdefghijklmnopqrstuvwxyz";
+      var charactersLength = characters.length;
+      var length = 5;
+      for (let i = 0; i < length; i++ ) {
+        pwLetter += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return pwLetter;
+    } else {
+      window.alert("Your password will not include lowercase.");
+      };
   }
 };
 
