a60d55610d4885538d4b7b742d7d316ce19bb33a
Jingsi Gong <gongjingsi@hotmail.com>
2022-07-18 23:46:29 -0400

    generate password based on input of length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,39 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// Get a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
+
+// Generate password based on criteria
+var generatePassword = function(){
+
+  var pwLength = window.prompt("Enter a number for the length of password at least 8 characters and no more than 128 characters. For example, 8.")
+  
+  if (Number.isInteger(parseInt(pwLength))) {
+    
+    password = new Array();
+
+    for(let i=0; i<pwLength; i++){
+      password[i] = randomNumber(0, 9);
+    }
+    return password.toString().replace(/,/g, '');
+
+  } else {
+    window.alert("You did not enter a number. Try again.");
+    
+  }
+};
+
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+    
 }
 
 // Add event listener to generate button
