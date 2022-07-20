428e61a8cde53e74fbbc70e8566861c6b4a2b733
Jingsi Gong <gongjingsi@hotmail.com>
2022-07-20 00:45:22 -0400

    refactor the coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var indicatorLower = 0;
 var indicatorUpper = 0;
 var indicatorSC = 0;
-var indicatorN = 1;
-var pwLength = 10; 
+var indicatorN = 0;
+var pwLength = 0; 
 
 var lowercaseLetters = "";
 var uppercaseLetters = "";
@@ -20,19 +20,24 @@ var randomNumber = function(min, max) {
 
 var passwordGenerator = function(pwLength){
   var pwLetter = " ";
-
-  if (indicatorLower){
-    lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-  };
-  if (indicatorUpper){
-    uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  };
-  if (indicatorSC) {
-    specialLetters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  };
-  if (indicatorN) {
-    numeric = "0123456789";
-  };
+  
+  if (indicatorLower === 0 && indicatorUpper===0 && indicatorSC===0 && indicatorN===0) {
+    window.alert("None of criteria is chosen! At least one character type should be included.");
+    generatePassword();
+  } else {
+    if (indicatorLower){
+      lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    };
+    if (indicatorUpper){
+      uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    };
+    if (indicatorSC) {
+      specialLetters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    };
+    if (indicatorN) {
+      numeric = "0123456789";
+    };
+  }
 
   var characters = lowercaseLetters + uppercaseLetters + specialLetters + numeric;
   var charactersLength = characters.length;
@@ -43,71 +48,94 @@ var passwordGenerator = function(pwLength){
   
   return pwLetter;
 };
+// Criteria confirm process
+var criteriaCOnfirm = function(){
+  pwConfirm = window.confirm("Ready to generate your password OR want to choose other criteria?");
+  if (pwConfirm) {
+    return passwordGenerator(pwLength);
+  } else {
+    generateCriteria();
+  }
+};
 
+// Generate password criteria
+var generateCriteria = function(){
+
+  var criteria = window.prompt("Choose which criteria to include in the password. Enter 'Lowercase' or 'Uppercase' or 'Numeric' or 'Special Character' to choose. ");
+
+  if (criteria == "Lowercase") {
+      var confirmL = window.confirm("Would you like to include lowercase in password?");
+      
+      if (confirmL) {
+        indicatorLower = 1;
+        
+        return criteriaCOnfirm();
+     
+      } else {
+        window.alert("Your password will not include lowercase. Please choose other criteria.");
+        generateCriteria();
+        };
+    } else if (criteria == "Uppercase") {
+      var confirmU = window.confirm("Would you like to include uppercase in password?");
+      
+      if (confirmU) {
+        indicatorUpper = 1;
+        return criteriaCOnfirm();
+      } else {
+        window.alert("Your password will not include uppercase. Please choose other criteria.");
+        generateCriteria();
+        };
+    } else if (criteria == "Special Character") {
+      var confirmSC = window.confirm("Would you like to include special character in password?");
+      
+      if (confirmSC) {
+        indicatorSC = 1;
+        return criteriaCOnfirm();
+      } else {
+        window.alert("Your password will not include special character. Please choose other criteria.");
+        generateCriteria();
+        };
+    } else if (criteria == "Numeric") {
+      var confirmN = window.confirm("Would you like to include numeric in password?");
+      
+      if (confirmN) {
+        indicatorN = 1;
+        return criteriaCOnfirm();
+      } else {
+        window.alert("Your password will not include numeric. Please choose other criteria.");
+        generateCriteria();
+        };
+    } else if (criteria == "" || criteria == null) {
+      pwConfirm = window.confirm("None of criteria is chosen! At least one character type should be included. Retry or quit?");
+      if (pwConfirm){
+        generateCriteria();
+      } else {
+        return;
+      }
+    } else {
+      window.alert("Please enter listed valid criteria to continue.");
+      generateCriteria();
+    }
+}
 // Generate password based on criteria
 var generatePassword = function(){
   
-  var criteria = window.prompt("Choose which criteria to include in the password. Enter 'Length' or 'Lowercase' or 'Uppercase' or 'Numeric' or 'Special Character' to choose. ");
-  
-  if (criteria == "Length") {
-    pwLength = window.prompt("Enter a number for the length of password at least 8 characters and no more than 128 characters. For example, 8.");
+  inputLength = window.prompt("Enter a number for the length of password at least 8 characters and no more than 128 characters. For example, 8.");
     
-    if (Number.isInteger(parseInt(pwLength))) {
-      return passwordGenerator(parseInt(pwLength));
+  if (Number.isInteger(parseInt(inputLength))) {
+    pwLength = inputLength;
+  } else {
+    confirmRetry = window.confirm("You did not enter a number. Try again?");
+    if (confirmRetry){
+      generatePassword();
     } else {
-      window.alert("You did not enter a number. Try again.");
-
+      return;
     }
-  } else if (criteria == "Lowercase") {
-    var confirmL = window.confirm("Would you like to include lowercase in password?");
-    
-    if (confirmL) {
-      indicatorLower = 1;
-      pwConfirm = window.confirm("Ready to generate your password OR want to choose other criteria?");
-      if (pwConfirm) {
-        return passwordGenerator(pwLength);
-      }
-    } else {
-      window.alert("Your password will not include lowercase. ");
-      };
-  } else if (criteria == "Uppercase") {
-    var confirmU = window.confirm("Would you like to include uppercase in password?");
-     
-    if (confirmU) {
-      indicatorUpper = 1;
-      pwConfirm = window.confirm("Ready to generate your password OR want to choose other criteria?");
-      if (pwConfirm) {
-        return passwordGenerator(pwLength);
-      }
-    } else {
-      window.alert("Your password will not include uppercase.");
-      };
-  } else if (criteria == "Special Character") {
-    var confirmSC = window.confirm("Would you like to include special character in password?");
-     
-    if (confirmSC) {
-      indicatorSC = 1;
-      pwConfirm = window.confirm("Ready to generate your password OR want to choose other criteria?");
-      if (pwConfirm) {
-        return passwordGenerator(pwLength);
-      }
-    } else {
-      window.alert("Your password will not include special character.");
-      };
-  } else if (criteria == "Numeric") {
-    var confirmN = window.confirm("Would you like to include numeric in password?");
-     
-    if (confirmN) {
-      indicatorN = 1;
-      pwConfirm = window.confirm("Ready to generate your password OR want to choose other criteria?");
-      if (pwConfirm) {
-        return passwordGenerator(pwLength);
-      }
-    } else {
-      window.alert("Your password will not include numeric.");
-      };
-  }
-};
+  };
+  
+  return generateCriteria();
+  
+  };
 
 // Write password to the #password input
 function writePassword() {
@@ -115,7 +143,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-    
+   
 }
 
 // Add event listener to generate button
