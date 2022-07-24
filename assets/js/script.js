d9653fc0a7fa16da2d670c01c57ac4fe580d4309
Jingsi Gong <gongjingsi@hotmail.com>
2022-07-24 09:59:16 -0400

    clean console.log and refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,26 @@
-// var indicatorLower = 0;
-// var indicatorUpper = 0;
-// var indicatorSC = 0;
-// var indicatorN = 0;
-// var pwLength = 0; 
+// Initiate indicator to determine if criteria needs to be included in password
 
 var indicatorLower = localStorage.getItem("indicatorLower");
 var lowercaseLetters = localStorage.getItem("lowercaseLetters");
 
 if (indicatorLower == null) {
-  // localStorage.setItem("indicatorLower", 0);
+  
   localStorage.setItem("lowercaseLetters", "");
 }
-
-console.log("init lowercaseIndicator: ", localStorage.getItem("indicatorLower"));
 
 var indicatorUpper = localStorage.getItem("indicatorUpper");
 var uppercaseLetters = localStorage.getItem("uppercaseLetters");
 
 if (indicatorUpper == null) {
-  // localStorage.setItem("indicatorUpper", 0);
+  
   localStorage.setItem("uppercaseLetters", "");
 }
-
-console.log("init uppercaseIndicator: ", localStorage.getItem("indicatorUpper"));
 
 var indicatorSC = localStorage.getItem("indicatorSC");
 var specialLetters = localStorage.getItem("specialLetters");
 
 if (indicatorSC == null) {
-  // localStorage.setItem("indicatorSC", 0);
+  
   localStorage.setItem("specialLetters", "");
 }
 
@@ -36,14 +28,9 @@ var indicatorN = localStorage.getItem("indicatorN");
 var numeric = localStorage.getItem("numeric");
 
 if (indicatorN == null) {
-  // localStorage.setItem("indicatorN", 0);
+  
   localStorage.setItem("numeric", "");
 }
-
-// var lowercaseLetters = "";
-// var uppercaseLetters = "";
-// var specialLetters = "";
-// var numeric = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -51,24 +38,10 @@ var generateBtn = document.querySelector("#generate");
 // Reset criteria indicator once user generated password
 var refreshCriteria = function () {
   localStorage.clear();
-
-  // localStorage.setItem("indicatorLower", null);
-  // localStorage.setItem("lowercaseLetters", "");
-  console.log("In refresh lowercaseIndicator: ", localStorage.getItem("indicatorLower"));
-  // localStorage.setItem("indicatorUpper", null);
-  // localStorage.setItem("uppercaseLetters", "");
-  console.log("In refresh uppercaseIndicator", localStorage.getItem("indicatorUpper"));
-  // localStorage.setItem("indicatorSC", null);
-  // localStorage.setItem("specialLetters", "");
-  console.log("In refresh SCIndicator", localStorage.getItem("indicatorSC"));
-  // localStorage.setItem("indicatorN", null);
-  // localStorage.setItem("numeric", "");
-  console.log("In refresh NIndicator", localStorage.getItem("indicatorN"));
 }
 
 // Get a random numeric value
 var passwordGenerator = function(pwLength){
-  
 
   var pwLetter = " ";
   
@@ -76,34 +49,27 @@ var passwordGenerator = function(pwLength){
     window.alert("None of criteria is chosen! At least one character type should be included.");
     generatePassword();
   } else {
-    console.log("check", localStorage.getItem("indicatorLower"));
+    
     if (localStorage.getItem("indicatorLower") !== null){
       localStorage.setItem("lowercaseLetters", "abcdefghijklmnopqrstuvwxyz");
     };
-    console.log("checkLL", localStorage.getItem("lowercaseLetters"));
-    console.log("check", localStorage.getItem("indicatorUpper"));
-    console.log("checkUL before", localStorage.getItem("uppercaseLetters"));
+
     if (localStorage.getItem("indicatorUpper") !== null){
       localStorage.setItem("uppercaseLetters", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
     };
-    console.log("checkUL", localStorage.getItem("uppercaseLetters"));
-    console.log("check", localStorage.getItem("indicatorSC"));
+
     if (localStorage.getItem("indicatorSC") !== null) {
       localStorage.setItem("specialLetters", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
     };
 
-    console.log("checkSC", localStorage.getItem("specialLetters"));
-    console.log("check", localStorage.getItem("indicatorN"));
     if (localStorage.getItem("indicatorN") !== null) {
       localStorage.setItem("numeric", "0123456789");
     };
 
-    console.log("checkN", localStorage.getItem("numeric"));
   }
   
+  // null value in js has been converted as string "null" so need to get rid of that
   var characters = [localStorage.getItem("lowercaseLetters"), localStorage.getItem("uppercaseLetters"), localStorage.getItem("specialLetters"), localStorage.getItem("numeric")].join("");
-  // var characters = localStorage.getItem("lowercaseLetters") + localStorage.getItem("uppercaseLetters") + localStorage.getItem("specialLetters") + localStorage.getItem("numeric");
-  console.log("variable", characters);
   var charactersLength = characters.length;
   
   for (let i=0; i < pwLength; i++){
@@ -112,19 +78,17 @@ var passwordGenerator = function(pwLength){
   
   return pwLetter;
 };
+
 // Criteria confirm process
 var criteriaConfirm = function(){
   pwConfirm = window.confirm("Ready to generate your password OR include another criteria?");
   if (pwConfirm) {
 
-    res = passwordGenerator(pwLength);
-    console.log(res);
-    
-    return res;
+    return passwordGenerator(pwLength);
     
   } else {
-    generateCriteria();
-  }
+    return generateCriteria();
+  };
 };
 
 // Generate password criteria
@@ -138,10 +102,6 @@ var generateCriteria = function(){
       if (confirmL) {
         localStorage.setItem("indicatorLower", 1);
         
-        // res = criteriaConfirm();
-        // console.log("generateCriteriaF_L", res);
-        // return res;
-     
       } else {
         window.alert("Your password will not include lowercase. Please choose other criteria.");
         generateCriteria();
@@ -152,9 +112,6 @@ var generateCriteria = function(){
       if (confirmU) {
         localStorage.setItem("indicatorUpper", 1);
 
-        // res = criteriaConfirm();
-        // console.log("generateCriteriaF_U", res);
-        // return res;
       } else {
         window.alert("Your password will not include uppercase. Please choose other criteria.");
         generateCriteria();
@@ -164,9 +121,6 @@ var generateCriteria = function(){
       
       if (confirmSC) {
         localStorage.setItem("indicatorSC", 1);
-        // res = criteriaConfirm();
-        // console.log("generateCriteriaF_SC", res);
-        // return res;
       } else {
         window.alert("Your password will not include special character. Please choose other criteria.");
         generateCriteria();
@@ -177,9 +131,6 @@ var generateCriteria = function(){
       if (confirmN) {
         localStorage.setItem("indicatorN", 1);
         
-        // res = criteriaConfirm();
-        // console.log("generateCriteriaF_N", res);
-        // return res;
       } else {
         window.alert("Your password will not include numeric. Please choose other criteria.");
         generateCriteria();
@@ -197,9 +148,8 @@ var generateCriteria = function(){
       generateCriteria();
     }
 
-  res = criteriaConfirm();
-  console.log("generateCriteriaF", res);
-  return res;
+  return criteriaConfirm();
+
 }
 // Generate password based on criteria
 var generatePassword = function(){
@@ -213,9 +163,7 @@ var generatePassword = function(){
     if (intLength > 7 && intLength < 129){
       pwLength = intLength;
 
-      res = generateCriteria();
-      console.log("generatePasswordF", res);
-      return res;
+      return generateCriteria();
 
     } else {
       confirmRetry = window.confirm("Your number does not sit in range specified. Try again?");
